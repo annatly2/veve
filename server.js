2b314af82df676a58a4d94b98bb667b69a908ce9
Kai Wells <support@kaiwells.me>
2017-11-21 18:10:17 -0800

    Moving to Handlebars; autologin from signup page; show password toggle

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var bodyparser = require("body-parser");
-var jwt = require("jwt-simple");
-var db = require("./models")
+var db = require("./models");
 
 var PORT = process.env.PORT || 8000;
 var app = express();
@@ -11,8 +11,18 @@ app.use(bodyparser.json());
 
 app.set("jwtSecret", process.env.JWT_SECRET || "JWT_TESTING");
 
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
 app.use(express.static("public"));
 app.use("/api", require("./api")(app));
+app.get("/", function(req, res) {
+  res.render("login", {layout: "landing"});
+})
+
+app.get("/signup", function(req, res) {
+  res.render("signup", {layout: "landing"});
+})
 
 var db_options = {};
 if (process.env.NODE_DB_ENV === "overwrite") {
