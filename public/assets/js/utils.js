63a9bfc3d2bbb968acd9e691bcdc8daf0bf5ccd4
Kai Wells <support@kaiwells.me>
2017-11-27 12:39:56 -0800

    Clean up outfits

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -73,11 +73,25 @@ function automaticTokenCheck() {
   );
 }
 
+function signOut() {
+  window.localStorage.removeItem("access_token");
+  window.location = "/";
+}
+
 function capitalize(word) {
   return word[0].toUpperCase() + word.slice(1);
 }
 
-function signOut() {
-  window.localStorage.removeItem("access_token");
-  window.location = "/";
+function shuffle(arr) {
+  // Fisher-Yates (Knuth) Algorithm
+  var shuffled = arr.slice(0); // Copy by value
+  for (var i = arr.length-1; i > 0; i--) {
+      // 1 <= i <= n-1
+      var j = Math.floor(Math.random()*(i+1)); // 0 <= j <= i
+      // Swap i and j, copying by value
+      var temp = shuffled[i];
+      shuffled[i] = shuffled[j];
+      shuffled[j] = temp;
+  }
+  return shuffled;
 }
