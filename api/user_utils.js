3ec8c7a82d962754fe68b6fd4b4a6dcb70d28fc1
Kai Wells <support@kaiwells.me>
2017-11-27 21:42:11 -0800

    Re-encrypt all garment images when user changes credentails

--- api/user_utils.js
+++ api/user_utils.js
@@ -1,6 +1,7 @@
 var cu = require("./crypto_utils");
 var models = require("../models");
 var User = models.User;
+var Garment = models.Garment;
 
 function getUser(email) {
   var emailHash = cu.hashSync(email);
@@ -44,10 +45,51 @@ module.exports = {
       .then(function(user) {
         if (user === null) throw new Error("user not found");
         var hashEmail = cu.hashSync(values.email);
-        var salt = cu.newSaltSync(values.email);
-        var makePassword = cu.hashPassword(values.password, salt);
-        var encryptUsername = cu.encrypt(values.username, salt);
-        return Promise.all([hashEmail, salt, makePassword, encryptUsername])
+        var oldSalt = user.salt;
+        var newSalt = cu.newSaltSync(values.email);
+        var makePassword = cu.hashPassword(values.password, newSalt);
+        var encryptUsername = cu.encrypt(values.username, newSalt);
+        var reEncryptImages = Garment.findAll({where: {UserId: user.id}})
+          .then(function(garments) {
+            var decrypts = [];
+            var ids = [];
+            for (var i = 0; i < garments.length; i++) {
+              var g = garments[i];
+              decrypts.push(cu.decrypt(g.dataValues.image, oldSalt));
+              ids.push(g.id);
+            }
+            return Promise.all(decrypts)
+              .then(function(plaintexts) {
+                var pairs = [];
+                for (var i = 0; i < plaintexts.length; i++) {
+                  pairs.push({
+                    image: plaintexts[i],
+                    garmentId: ids[i]
+                  })
+                }
+                return pairs
+              })
+          })
+          .then(function(plaintextPairs) {
+            var encrypts = [];
+            var ids = [];
+            for (var i = 0; i < plaintextPairs.length; i++) {
+              var p = plaintextPairs[i];
+              encrypts.push(cu.encrypt(p.image, newSalt))
+              ids.push(p.garmentId)
+            }
+            return Promise.all(encrypts)
+              .then(function(ciphertexts) {
+                var updateGarments = [];
+                for (var i = 0; i < ciphertexts.length; i++) {
+                  var payload = {image: ciphertexts[i]};
+                  updateGarments.push(Garment.update(payload, {where: {id: ids[i]}}))
+                }
+                return Promise.all(updateGarments);
+              })
+          })
+        var promises = [hashEmail, newSalt, makePassword, encryptUsername, reEncryptImages];
+        return Promise.all(promises)
           .then(function(values) {
             return user.update({
               email:    values[0],
